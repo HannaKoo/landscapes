96e0eff8ff5a3b2bcbe82d792692cfd9a9f90caf
Jere Kupari <jere.kupari@outlook.com>
2023-01-10 20:31:45 +0200

    issuers

--- Code/issuers.py
+++ Code/issuers.py
@@ -4,4 +4,7 @@ tree = ET.parse('../Data/dataset_landscape.xml')
 root = tree.getroot()
 
 for i in root:
-  print(i.tag, i.get('nr'))
+  print(i.tag, i.get('nr'), end = ' ')
+  for j in i.iter(issuer):
+    print(j.tag, end = ' ')
+  print()
