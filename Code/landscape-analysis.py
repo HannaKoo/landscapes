8fd07a3ad3de0ac41b78eae7c2c12fa81b385a43
Hanna-Mari <75001761+HannaKoo@users.noreply.github.com>
2023-01-23 19:37:50 +0200

    list-of-dicts

--- Code/landscape-analysis.py
+++ Code/landscape-analysis.py
@@ -1,5 +1,5 @@
-# import numpy as np
-# import pandas as pd
+import numpy as np
+import pandas as pd
 import xml.etree.ElementTree as ET
 
 tree = ET.parse('Data/dataset_landscape.xml')
@@ -21,22 +21,22 @@ both
 
 """ Python tabular data
 pandas https://outerbounds.com/docs/python-tabular-data-structures/ 
-Esim. dict of lists
+Esim. dict of lists,
+tai list of dicts
 """
-data = {'nr':[], 'landsc':[], 'iss-stat':[], 'iss-gen':[]}
+# data = {'nr':[], 'landsc':[], 'iss-stat':[], 'iss-gen':[]}
+# data = [{'nr': '01', 'gender': 'm', 'n_landscapes': 4}]
 
 # doc.issuer.gender
 # doc.issuer.status
 # doc.elements.sum
 
-amounts = []
+# amounts = []
+data = []
 
 for doc in root:
-  amounts.append(0)
-  print(doc.get('nr'))
-  print(doc.tag, doc.get('nr'), end = ' ')
+  data.append({'nr': doc.get('nr'), 'landscapes': 0})
   for j in doc.iter('landscape'):
-    amounts[len(amounts)-1] += 1
-  print()
+    data[-1]['landscapes'] += 1
   
-print(amounts)
+print(data)
