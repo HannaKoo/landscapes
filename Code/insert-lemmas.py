7b072698a6082ba5c147252746a5fc10d9aa9c1a
Hanna-Mari <75001761+HannaKoo@users.noreply.github.com>
2023-02-09 12:47:11 +0200

    Read lemmas file

--- Code/insert-lemmas.py
+++ Code/insert-lemmas.py
@@ -1,5 +1,22 @@
+# import xml.etree.ElementTree as ET
 
+# tree = ET.parse('Data/dataset_landscape.xml')
+# root = tree.getroot()
 
-lemmas = 
+# # replaces CDATA's with &amp;'s but 
+# https://stackoverflow.com/questions/55217892/keeping-cdata-sections-while-parsing-through-xml
+# tree.write("Data/dataset_test_etree.xml", encoding="UTF-8", short_empty_elements=False)
 
-for lemma in lemmas:
+lemmaFile = open('Data/landscape_lemmas.txt', 'r', encoding="utf-8")
+
+lemmas = lemmaFile.read().split('\n')
+lemmas = [i.split(',') for i in lemmas]
+
+# https://docs.python.org/3/howto/regex.html#modifying-strings
+
+for i in lemmas:
+  print(i)
+
+# lemmas[0] -> lemma="lemmas[1]"
+
+# for lemma in lemmas:
