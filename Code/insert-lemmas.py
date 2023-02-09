506f9dbedcd977e011514985c19c8477509a89cd
Jere Kupari <jere.kupari@outlook.com>
2023-02-09 20:21:18 +0200

    String methods

--- Code/insert-lemmas.py
+++ Code/insert-lemmas.py
@@ -10,13 +10,10 @@ import re
 
 lemmaFile = open('Data/landscape_lemmas.txt', 'r', encoding="utf-8")
 
-lemmas = lemmaFile.read().split('\n')
+lemmas = lemmaFile.read().splitlines()
 lemmas = [i.split(',') for i in lemmas]
 
 # https://docs.python.org/3/howto/regex.html#modifying-strings
-
-for i in lemmas:
-  print(i)
 
 # lemmas[0] -> lemma="lemmas[1]"
 with open('Data/dataset_landscape.xml', 'r', encoding="utf-8") as f:
@@ -25,9 +22,11 @@ with open('Data/dataset_landscape.xml', 'r', encoding="utf-8") as f:
 # regex: r'type="()">'+lemma+r'</landscape>'
 # find type="church">kirkon</landscape>
 # replace type="church" lemma="kirkko">kirkon</landscape>
+# str.find(sub[, start[, end]]) 
+# str.replace(old, new[, count]) 
 
 for lemma in lemmas:
-  
+  print(lemma)
 
 
 #  p = re.compile(r'type="()">') 
