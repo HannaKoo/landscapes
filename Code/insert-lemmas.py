f485ff100123c36708ae44c22f0442ff9c3f9574
Hanna-Mari <75001761+HannaKoo@users.noreply.github.com>
2023-02-11 14:00:26 +0200

    Add comments and warnings

--- Code/insert-lemmas.py
+++ Code/insert-lemmas.py
@@ -2,6 +2,7 @@ with open('Data/landscape_lemmas.txt', 'r', encoding="utf-8") as f:
   lemmas = f.read().splitlines()
 lemmas = [i.split(',') for i in lemmas]
 
+# Remove duplicate lemmas. TODO: Give a warning when lemma[0] are same but rest of the lines differ.
 lemmas = set(map(tuple, lemmas))
 
 # for lemma in sorted(lemmas):
@@ -15,8 +16,10 @@ with open('Data/dataset_landscape.xml', 'r', encoding="utf-8") as f:
 # str.find(sub[, start[, end]]) 
 # str.replace(old, new[, count]) 
 
+# Warning: If there are lemma with the same lemma[0], but differences in lemma[1] or lemma[2], the lemmas will be added twice, so check for things like: 'lemma="gathan" lemma="gatan">'
 for lemma in lemmas:
   data = data.replace('>'+lemma[0]+'</landscape>', ' lemma="'+lemma[1]+'">'+lemma[0]+'</landscape>')
 
+# Warning: Overwrites your data, so have your git and backups ready!
 with open('Data/dataset_landscape.xml', 'w', encoding="utf-8") as f:
   f.write(data)
