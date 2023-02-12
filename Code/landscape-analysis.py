5f33959e6f8de06fa4bef0afc8345f3c92f84b49
Hanna-Mari <75001761+HannaKoo@users.noreply.github.com>
2023-02-12 12:30:27 +0200

    Count words and landscapes by language

--- Code/landscape-analysis.py
+++ Code/landscape-analysis.py
@@ -1,5 +1,3 @@
-# import numpy as np
-# import pandas as pd
 import xml.etree.ElementTree as ET
 
 tree = ET.parse('Data/dataset_landscape.xml')
@@ -11,7 +9,6 @@ doc   iss.gender   iss.status landscape-elements
 02    both         burg       3
 """
 
-
 """ Numbers by document:
      noble ecc   burg  peas
 m     
@@ -19,30 +16,26 @@ f
 both  
 """
 
-""" Python tabular data
-pandas https://outerbounds.com/docs/python-tabular-data-structures/ 
-Esim. dict of lists,
-tai list of dicts
-"""
+# dict of lists vs list of dicts:
 # data = {'nr':[], 'landsc':[], 'iss-stat':[], 'iss_gen':[]}
 # data = [{'nr': '01', 'gender': 'm', 'n_landscapes': 4}]
 
-# doc.issuer.gender
-# doc.issuer.status
-# doc.elements.sum
-
-# amounts = []
 data = []
 
+### Collect all data:
 for doc in root:
   
-  data.append({'nr': doc.get('nr'), 'iss_gen': None, 'landscapes': 0})
-  
-  for j in doc.iter('issuer'):
+  data.append({'nr': doc.get('nr'), 'lang': None, 'when': doc.get('when'),
+               'words': 0, 'iss_gen': None, 'landscapes': 0})
+
+  for edition in doc.iter('edition'):
+    data[-1]['lang'] = edition.attrib['{http://www.w3.org/XML/1998/namespace}lang']
+
+  for issuer in doc.iter('issuer'):
     old = data[-1]['iss_gen']
     if old == None:
-      data[-1]['iss_gen'] = j.get('gender')
-    elif old != j.get('gender'):
+      data[-1]['iss_gen'] = issuer.get('gender')
+    elif old != issuer.get('gender'):
       data[-1]['iss_gen'] = 'both'
     # (else do nothing)
 
@@ -57,10 +50,14 @@ for doc in root:
     #   gender != None/'f'/'m'/'both'
     # eg. 'm','m?' -> 'both'
 
-  for j in doc.iter('landscape'):
+  for edition in doc.iter('edition'):
+    data[-1]['words'] += len("".join(edition.itertext()).split())
+
+  for landscape in doc.iter('landscape'):
     data[-1]['landscapes'] += 1
   
 
+### Print and analyse data:
 for i in data:
   print(i)
 
@@ -92,8 +89,27 @@ for i in data:
 print(landscape_sums)
 
 print()
-print('Means:')
+print('Landscape means:')
 print("'f': ", landscape_sums['f']/doc_sums['f'])
 print("'m': ", landscape_sums['m']/doc_sums['m'])
 print("'both': ", landscape_sums['both']/doc_sums['both'])
 print("'all':", landscape_sums['all']/len(data))
+
+
+decades = {}
+for i in range(137, 152):
+  decades[str(i*10)] = {'words': {'la': 0, 'sv': 0}, 'landscapes': 0}
+
+for doc in data:
+  decade = doc['when'][:3]+'0'
+  decades[decade]['landscapes'] += doc['landscapes']
+  if doc['lang'] == 'la':
+    decades[decade]['words']['la'] += doc['words']
+  if doc['lang'][:2] == 'sv':
+    decades[decade]['words']['sv'] += doc['words']
+
+
+
+print(decades)
+# for decade in decades:
+#   print(decade, ':', decade['words'], decade['landscapes'])
