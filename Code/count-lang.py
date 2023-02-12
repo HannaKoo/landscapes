ba9c107896c5a6d8d5edb3686e56c70ca6c7788b
Hanna-Mari <75001761+HannaKoo@users.noreply.github.com>
2023-02-12 08:38:32 +0200

    Comment and cleaning

--- Code/count-lang.py
+++ Code/count-lang.py
@@ -11,17 +11,21 @@ words = {'la':0, 'sv':0}
 # .get() does not work with xml:lang, relevant search terms: 
 #  namespace, nsmap
 
+# Counting words with .itertext does not include child elements, eg. landscape elements
+
 for doc in root:
-  print(doc.get('nr'), end=" ")
+  print(doc.tag, doc.get('nr'), end=" ")
   for edition in doc.iter('edition'):
     lang = edition.attrib['{http://www.w3.org/XML/1998/namespace}lang']
     print(lang)
+    wordcount = len("".join(edition.itertext()).split())
+    print(wordcount)
     if lang[:2] == 'la':
       langs['la'] += 1
-      words['la'] += len("".join(edition.itertext()).split())
+      words['la'] += wordcount
     elif lang[:2] == 'sv':
       langs['sv'] += 1
-      words['sv'] += len("".join(edition.itertext()).split())
+      words['sv'] += wordcount
     else:
       print("Found", lang)
       print("Found", edition.attrib)
