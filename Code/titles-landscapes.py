0db26a5b9f4cd34d8605c166bfced7e454f3aee0
Jere Kupari <jere.kupari@outlook.com>
2023-05-04 22:15:50 +0300

    Separate repeating, complex logic into a function

--- Code/titles-landscapes.py
+++ Code/titles-landscapes.py
@@ -4,26 +4,22 @@ import xml.etree.ElementTree as ET
 tree = ET.parse('Data/dataset_landscape.xml')
 root = tree.getroot()
 
+containing_tag = 'title'
+tags_of_interest = ['landscape', 'transaction']
+
+def print_content(elem):
+  for tag in tags_of_interest:
+    for j in elem.iter(tag):
+      everything = "".join(j.itertext())
+      beginning = j.text
+      if  everything == beginning:
+        print(tag+": ", everything)
+      else:
+        print("WARNING:", everything, "=!", beginning)
+
 for doc in root:
   print(doc.tag, doc.get('nr'))#, end=" ")
-
-  for title in doc.iter('title'):
-    print("".join(title.itertext()))
-
-    for landscape in title.iter('landscape'):
-      everything = "".join(landscape.itertext())
-      beginning = landscape.text
-      if  everything == beginning:
-        print("LANDSCAPE: ", everything)
-      else:
-        print("WARNING:", everything, "=!", beginning)
-
-    for transaction in title.iter('transaction'):
-      everything = "".join(transaction.itertext())
-      beginning = transaction.text
-      if  everything == beginning:
-        print("TRANSACTION: ", everything)
-      else:
-        print("WARNING:", everything, "=!", beginning)
-
+  for containing_elem in doc.iter(containing_tag):
+    print("".join(containing_elem.itertext()))
+    print_content(containing_elem)
   print()
