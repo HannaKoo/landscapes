e45a86a789c61e68e26e58fa1b0a3f39723f19ac
Hanna-Mari Kupari <hmknie@utu.fi>
2023-05-05 10:33:07 +0300

    Confirmed the note-bug also in doc 25

--- Code/listing_titles.py
+++ Code/listing_titles.py
@@ -38,7 +38,8 @@ for title in root.findall('.//title'):
         title.remove(note)
 # Seems to work!
 # EXCEPT: For some reason doc 12 is missing the whole title after marking a <note> in the beginning. 
-# Are there other problems like this?
+# Are there other problems like this? 
+# Yes, doc 25 printing starts only from the first tag after the note(transaction)!
 # TODO: remove translations, what else?
 
 # for translation in root.iter('translation'):
