bba6585039eebbd3201e3007187647cbde47e6cb
Jere Kupari <jere.kupari@outlook.com>
2023-05-11 21:15:25 +0300

    gender-type done

--- Code/gender-type.py
+++ Code/gender-type.py
@@ -1,43 +1,62 @@
 # List landscape @types by issuer gender, only in edition, skip titles
 # Landscapes found at least in edition/landscape and edition/p/landscape,
-# others?
+# others? 
+# TODO: How to list where landscapes are found? landscape.parent(.tag)...?
 
 import xml.etree.ElementTree as ET
 
 tree = ET.parse('Data/dataset_landscape.xml')
 root = tree.getroot()
 
+# Flatten a list of lists, goes only two levels deep.
 def flatten(list):
-  flattened = []
-  for sublist in list:
-    for item in sublist:
-        flattened.append(item)
-  return flattened
+    flattened = []
+    for sublist in list:
+        for item in sublist:
+            flattened.append(item)
+    return flattened
 
-data = []
+        # Counts for types:
+        # type   f  m both
+        # river 11 22 14
 
+data = []  # tobe a list of dicts
 for doc in root:
-  
-  data.append({'nr': doc.get('nr'), 'iss_gen': None, 'types': []})
+    data.append({'nr': doc.get('nr'), 'iss_gen': None, 'types': []})
+    for issuer in doc.iter('issuer'):
+        old = data[-1]['iss_gen']
+        if old == None:
+            data[-1]['iss_gen'] = issuer.get('gender')
+        elif old != issuer.get('gender'):
+            data[-1]['iss_gen'] = 'both'
+    
+    for landscape in doc.iterfind('edition/p/landscape'):
+        (data[-1]['types']).append(landscape.get('type'))
+    for landscape in doc.iterfind('edition/landscape'):
+        (data[-1]['types']).append(landscape.get('type'))
 
-  for issuer in doc.iter('issuer'):
-    old = data[-1]['iss_gen']
-    if old == None:
-      data[-1]['iss_gen'] = issuer.get('gender')
-    elif old != issuer.get('gender'):
-      data[-1]['iss_gen'] = 'both'
-  
-  # Is there a way to do these in one go? 
-  # 'edition//landscape' not working (gives also titles).
-  # 'edition/*/landscape' does something weird.
-  # union of the .iterfinds? Needs .findall?
-  for landscape in doc.iterfind('edition/p/landscape'):
-    (data[-1]['types']).append(landscape.get('type'))
-  for landscape in doc.iterfind('edition/landscape'):
-    (data[-1]['types']).append(landscape.get('type'))
-#   for landscape in doc.iterfind('edition/*/landscape'):
-#     (data[-1]['types']).append(landscape.get('type'))
+typeset = set()
+for i in data:
+    for type in i.get('types'):
+        typeset.add(type)
+print('typeset', typeset)
+alltypes = dict.fromkeys(typeset, 0)
+print('alltypes', alltypes)
 
+f_types = alltypes.copy()
+m_types = alltypes.copy()
+both_types = alltypes.copy()
+for i in data:
+    if i.get('iss_gen') == 'f':
+        for type in i.get('types'):
+            f_types[type] += 1
+    if i.get('iss_gen') == 'm':
+        for type in i.get('types'):
+            m_types[type] += 1
+    if i.get('iss_gen') == 'both':
+        for type in i.get('types'):
+            both_types[type] += 1
+    
 
 ###  Write file; analyse data:  ###
 
@@ -48,7 +67,6 @@ with open('Results/from_scripts/gender-type.txt', 'w', encoding='utf8') as f:
     f.write('\n')
     f.write("documents:" + str(len(data)))
 
-
     f.write('\n'*2)
     f.write("types by gender:\n")
 
@@ -58,29 +76,25 @@ with open('Results/from_scripts/gender-type.txt', 'w', encoding='utf8') as f:
 
     f.write('f: ' + str(types['f']) + '\n')
     f.write(str(sum(map(len, (types['f'])))) + '\n'*2)
-
     f.write('m: ' + str(types['m']) + '\n')
     f.write(str(sum(map(len, (types['m'])))) + '\n'*2)
-
     f.write('both: ' + str(types['both']) + '\n')
     f.write(str(sum(map(len, (types['both'])))) + '\n'*2)
 
     ## Flatten lists and count types
-
     f_flat = flatten(types['f'])
     m_flat = flatten(types['m'])
     both_flat = flatten(types['both'])
 
-    # Counts for types:
-    # type   f  m both
-    # river 11 22 14
-
-# def count_items(list):
     f_types = {}
 
     for type in f_flat:
-      if type in f_types:
-          f_types[type] += 1
-      else:
-          f_types[type] = 1
+        if type in f_types:
+            f_types[type] += 1
+        else:
+            f_types[type] = 1
     f.write('f :' + str(f_types) + '\n')
+
+    print('f', f_types)
+    print('m', m_types)
+    print('both', both_types)
