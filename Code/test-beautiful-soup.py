7080268f48f1b18ee3df4db662c365a606099cb8
Jere Kupari <jere.kupari@outlook.com>
2023-05-14 13:40:11 +0300

    xml:lang works, but not inherited

--- Code/test-beautiful-soup.py
+++ Code/test-beautiful-soup.py
@@ -7,4 +7,9 @@ root = tree.getroot()
 with open('Data/dataset_landscape.xml', 'r', encoding="utf8") as fp:
     soup = BeautifulSoup(fp, 'xml')
 
-print(soup.landscapes.document)
+print(soup.landscapes.document.regesta.get('editor'))
+print(soup.landscapes.document.regesta.get('{http://www.w3.org/XML/1998/namespace}lang'))
+print(soup.landscapes.document.regesta.get('xml:lang'))
+print(soup.landscapes.document.regesta.attrs)
+print(soup.landscapes.document.regesta.folio.get('xml:lang'))
+print(soup.landscapes.document.regesta.folio.attrs)
