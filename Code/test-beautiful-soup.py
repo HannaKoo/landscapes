80ec366101cde5efe14a86d5d86ba0028ffc06b5
Jere Kupari <jere.kupari@outlook.com>
2023-05-14 14:38:27 +0300

    Beautiful Soup vs ElementTree

--- Code/test-beautiful-soup.py
+++ Code/test-beautiful-soup.py
@@ -7,9 +7,23 @@ root = tree.getroot()
 with open('Data/dataset_landscape.xml', 'r', encoding="utf8") as fp:
     soup = BeautifulSoup(fp, 'xml')
 
+print(" --- Beautiful Soup ---")
 print(soup.landscapes.document.regesta.get('editor'))
 print(soup.landscapes.document.regesta.get('{http://www.w3.org/XML/1998/namespace}lang'))
 print(soup.landscapes.document.regesta.get('xml:lang'))
 print(soup.landscapes.document.regesta.attrs)
 print(soup.landscapes.document.regesta.folio.get('xml:lang'))
 print(soup.landscapes.document.regesta.folio.attrs)
+print(" --- ElementTree ---")
+print(root.find('document/regesta').get('editor'))
+print(root.find('document/regesta').get('{http://www.w3.org/XML/1998/namespace}lang'))
+print(root.find('document/regesta').get('xml:lang'))
+print(root.find('document/regesta').attrib)
+print(root.find('document/regesta/folio').get('xml:lang'))
+print(root.find('document/regesta/folio').attrib)
+print(" --- Inherit? --- ")
+print(root.find('document/regesta').get('{http://www.w3.org/XML/1998/namespace}lang'))
+print(root.find('document/regesta/folio').get('{http://www.w3.org/XML/1998/namespace}lang'))
+
+# No XPath support in bs
+# print(soup.landscapes.document.regesta.folio.get('ancestor-or-self::*[attribute::xml:lang][1]/@xml:lang'))
