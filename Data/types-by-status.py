24407a2195141d35a94f4441c432e41ac95f3a15
Jere Kupari <jere.kupari@outlook.com>
2023-05-16 13:22:35 +0300

    updated spec

--- Data/types-by-status.py
+++ Data/types-by-status.py
@@ -1,8 +1,14 @@
 # Count types by status. Not inside titles.
-    # type    noble eccles  burgher peasant
-    # river     x       y       z       w
+    # type    noble eccles  burgher peasant several
+    # river     x       y       z       w       v
 
 import lxml.etree as ET
 tree = ET.parse('Data/dataset_landscape.xml')
 root = tree.getroot()
 
+for doc in root:
+    pass
+
+
+with open('Results/from_scripts/types-by-status.txt', 'w', encoding='utf8') as f:
+    pass
