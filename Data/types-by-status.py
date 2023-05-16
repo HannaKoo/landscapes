9ef8045438719b0393f1c1f961c7328ff54c1117
Jere Kupari <jere.kupari@outlook.com>
2023-05-16 14:02:12 +0300

    Data collected

--- Data/types-by-status.py
+++ Data/types-by-status.py
@@ -1,4 +1,5 @@
-# Count types by status. Not inside titles.
+# Count types by status.
+# Not inside titles.
     # type    noble eccles  burgher peasant several
     # river     x       y       z       w       v
 
@@ -6,9 +7,24 @@ import lxml.etree as ET
 tree = ET.parse('Data/dataset_landscape.xml')
 root = tree.getroot()
 
+types = {'eccle': [], 'noble': [], 'burgh': [], 'peasa': [], 'sever': []}
 for doc in root:
-    pass
+    # get issuer statuses and combine into one status:
+    statuses = set()
+    for issuer in doc.iter('issuer'):
+        status = issuer.get('status')[:5]
+        statuses.add(status)
+        if len(statuses) > 1:
+            status = 'sever'
+        print(statuses, status)
 
+    # get landscape types to types[status]:
+    for landscape in doc.iter('landscape'):
+        types[status].append(landscape.get('type'))
+
+for status in ('eccle', 'noble', 'burgh', 'peasa', 'sever'):
+    print(status)
+    print(types[status])
 
 with open('Results/from_scripts/types-by-status.txt', 'w', encoding='utf8') as f:
     pass
