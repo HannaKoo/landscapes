e3c5fb6a2e38a44ef985546fe39705d5e7d3cef0
Jere Kupari <jere.kupari@outlook.com>
2023-05-16 23:13:39 +0300

    Output a table

--- Code/types-by-status.py
+++ Code/types-by-status.py
@@ -19,7 +19,7 @@ for doc in root:
         if len(statuses) > 1:
             status = 'sever'
 
-    # get landscape types to types[status]:
+    # get landscape types, outside titles, to types[status]:
     for landscape in doc.iterfind('edition/p/landscape'):
         types[status].append(landscape.get('type'))
     for landscape in doc.iterfind('edition/landscape'):
@@ -32,10 +32,19 @@ for status in types:
 typeset = set()
 for status in types:
     typeset.update(types[status])
+# TODO: Would be better if typeset was sorted list, for stable results.
 print (typeset)
 
-
 with open('Results/from_scripts/types-by-status.txt', 'w', encoding='utf8') as f:
-    for status in ('eccle', 'noble', 'burgh', 'peasa', 'sever'):
+    for status in ('noble', 'eccle', 'burgh', 'peasa', 'sever'):
         print(status+":", file=f)
         print(counts[status], file=f)
+
+    print(file=f)
+    print("type", "rälssi", "kirkollinen", "porvari", "talonpoika", "useita", 
+          sep='\t', file=f)
+    for type in typeset:
+        f.write(type)
+        for status in ('noble', 'eccle', 'burgh', 'peasa', 'sever'):
+            f.write('\t'+ str(counts[status][type]))
+        f.write('\n')
