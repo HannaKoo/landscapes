2af9f43a14dcd170eb8549dde950445c8e9d57b9
Jere Kupari <jere.kupari@outlook.com>
2023-05-16 22:48:49 +0300

    Counting works

--- Code/types-by-status.py
+++ Code/types-by-status.py
@@ -3,12 +3,13 @@
     # type    noble eccles  burgher peasant several
     # river     x       y       z       w       v
 
+from collections import Counter
 # import lxml.etree as ET
 import xml.etree.ElementTree as ET
 tree = ET.parse('Data/dataset_landscape.xml')
 root = tree.getroot()
 
-types = {'eccle': [], 'noble': [], 'burgh': [], 'peasa': [], 'sever': []}
+types = {'noble': [], 'eccle': [], 'burgh': [], 'peasa': [], 'sever': []}
 for doc in root:
     # get issuer statuses and combine into one status:
     statuses = set()
@@ -17,7 +18,6 @@ for doc in root:
         statuses.add(status)
         if len(statuses) > 1:
             status = 'sever'
-        print(statuses, status)
 
     # get landscape types to types[status]:
     for landscape in doc.iterfind('edition/p/landscape'):
@@ -25,9 +25,17 @@ for doc in root:
     for landscape in doc.iterfind('edition/landscape'):
         types[status].append(landscape.get('type'))
 
+counts = {}
+for status in types:
+    counts[status] = Counter(types[status])
+
+typeset = set()
+for status in types:
+    typeset.update(types[status])
+print (typeset)
 
 
 with open('Results/from_scripts/types-by-status.txt', 'w', encoding='utf8') as f:
     for status in ('eccle', 'noble', 'burgh', 'peasa', 'sever'):
-        print(status, file=f)
-        print(types[status], file=f)
+        print(status+":", file=f)
+        print(counts[status], file=f)
